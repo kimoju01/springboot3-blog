53cbe05c27da91eb159ab8680b20a6638d8d5b46
Hyeju <kimoju03@naver.com>
2024-01-11 18:32:15 +0900

    Initial commit

---
+++ src/test/java/me/hyeju/springboot3blog/Springboot3BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hyeju.springboot3blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
