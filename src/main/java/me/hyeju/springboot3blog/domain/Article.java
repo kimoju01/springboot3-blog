1586abed260154ab43b0c0c48d0e5c2e9176d96f
Hyeju <kimoju03@naver.com>
2024-01-15 17:25:09 +0900

    feat: Article 엔티티 작성

---
+++ src/main/java/me/hyeju/springboot3blog/domain/Article.java
@@ -0,0 +1,31 @@
+package me.hyeju.springboot3blog.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)  // 접근 제어자가 protected인 기본 생성자 생성
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // 기본 키 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
