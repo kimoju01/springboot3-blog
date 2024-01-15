55f578819814312b2af0660fe30061e809ceef48
Hyeju <kimoju03@naver.com>
2024-01-15 17:28:10 +0900

    feat: BlogService save() 메서드 작성

---
+++ src/main/java/me/hyeju/springboot3blog/service/BlogService.java
@@ -0,0 +1,19 @@
+package me.hyeju.springboot3blog.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hyeju.springboot3blog.domain.Article;
+import me.hyeju.springboot3blog.dto.AddArticleRequest;
+import me.hyeju.springboot3blog.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+}
