44e2809ffc3bd6947b2cba8b1ff1ecfa58574955
Hyeju <kimoju03@naver.com>
2024-01-16 18:00:42 +0900

    feat: 글 하나 조회 findById() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/service/BlogService.java
+++ src/main/java/me/hyeju/springboot3blog/service/BlogService.java
@@ -7,6 +7,7 @@ import me.hyeju.springboot3blog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
@@ -21,6 +22,11 @@ public class BlogService {
     // 전체 글 조회
     public List<Article> findAll() {
         return blogRepository.findAll();
+    }
+
+    // 글 하나 조회
+    public Article findById(Long id) {
+        return blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
     }
 
 }
