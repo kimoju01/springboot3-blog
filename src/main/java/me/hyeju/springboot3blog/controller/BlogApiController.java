0c8f5a5d2c16e377014b79a9c6756e0400ea99bb
Hyeju <kimoju03@naver.com>
2024-01-16 16:00:56 +0900

    feat: GET /api/articles 전체 글 조회 findAllArticles() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/controller/BlogApiController.java
+++ src/main/java/me/hyeju/springboot3blog/controller/BlogApiController.java
@@ -3,12 +3,17 @@ package me.hyeju.springboot3blog.controller;
 import lombok.RequiredArgsConstructor;
 import me.hyeju.springboot3blog.domain.Article;
 import me.hyeju.springboot3blog.dto.AddArticleRequest;
+import me.hyeju.springboot3blog.dto.ArticleResponse;
 import me.hyeju.springboot3blog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController     // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러(Controller + ResponseBody)
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
@@ -24,6 +29,18 @@ public class BlogApiController {
 
         // 응답코드를 201로 설정하고 저장된 Article 객체를 응답 본문에 담아 클라이언트에 반환
         return ResponseEntity.status(HttpStatus.CREATED).body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        // 스트림으로 ArticleResponse 객체로 변환한 뒤 List로 수집
+        // ArticleResponse 생성자 참조해서 Article을 ArticleResponse로 매핑
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok().body(articles);
     }
 
 }
