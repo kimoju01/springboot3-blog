07279809695b51d6078cc90ff0ed069e6036eeca
Hyeju <kimoju03@naver.com>
2024-01-16 15:59:22 +0900

    feat: 전체 글 조회 findAll() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/service/BlogService.java
+++ src/main/java/me/hyeju/springboot3blog/service/BlogService.java
@@ -6,6 +6,8 @@ import me.hyeju.springboot3blog.dto.AddArticleRequest;
 import me.hyeju.springboot3blog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 public class BlogService {
@@ -14,6 +16,11 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    // 전체 글 조회
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 
 }
