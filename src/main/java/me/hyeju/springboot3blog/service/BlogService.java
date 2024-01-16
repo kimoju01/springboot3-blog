bac9df8ab2de894b8ae5d5d64c4568d9b70e863d
Hyeju <kimoju03@naver.com>
2024-01-16 20:18:42 +0900

    feat: 글 수정 update() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/service/BlogService.java
+++ src/main/java/me/hyeju/springboot3blog/service/BlogService.java
@@ -3,8 +3,10 @@ package me.hyeju.springboot3blog.service;
 import lombok.RequiredArgsConstructor;
 import me.hyeju.springboot3blog.domain.Article;
 import me.hyeju.springboot3blog.dto.AddArticleRequest;
+import me.hyeju.springboot3blog.dto.UpdateArticleRequest;
 import me.hyeju.springboot3blog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +34,16 @@ public class BlogService {
     // 글 삭제
     public void delete(Long id) {
         blogRepository.deleteById(id);
+    }
+
+    // 글 수정
+    @Transactional
+    public Article update(Long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+        // 여기서 return blogRepository.save(article); 하지 않아도 업데이트 되는 이유? -> JPA 더티 체킹!
     }
 
 }
