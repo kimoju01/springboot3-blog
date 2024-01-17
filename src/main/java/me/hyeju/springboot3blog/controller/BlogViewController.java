83a39b039d0de71271ab2a53b410a81ac4b2fcde
Hyeju <kimoju03@naver.com>
2024-01-17 20:57:30 +0900

    feat: GET /articles/{id} 글 하나 조회 getArticle() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/controller/BlogViewController.java
+++ src/main/java/me/hyeju/springboot3blog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.hyeju.springboot3blog.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.hyeju.springboot3blog.domain.Article;
 import me.hyeju.springboot3blog.dto.ArticleListViewResponse;
+import me.hyeju.springboot3blog.dto.ArticleViewResponse;
 import me.hyeju.springboot3blog.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -25,6 +28,15 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 
 }
