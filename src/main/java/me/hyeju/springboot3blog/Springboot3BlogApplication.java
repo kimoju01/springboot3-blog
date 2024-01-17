c7576a9cff1efc0be088f6b815841854a77b5c10
Hyeju <kimoju03@naver.com>
2024-01-17 20:34:10 +0900

    feat: EnableJpaAuditing 추가

--- src/main/java/me/hyeju/springboot3blog/Springboot3BlogApplication.java
+++ src/main/java/me/hyeju/springboot3blog/Springboot3BlogApplication.java
@@ -2,8 +2,10 @@ package me.hyeju.springboot3blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Springboot3BlogApplication {
 
     public static void main(String[] args) {
