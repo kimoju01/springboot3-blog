b4f1497b31287aa8cc98f0b5e99a752f393f27d8
Hyeju <kimoju03@naver.com>
2024-01-23 18:14:46 +0900

    feat: GET /new-article 글 생성, 수정 newArticle() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/controller/BlogViewController.java
+++ src/main/java/me/hyeju/springboot3blog/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -37,6 +38,18 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {    // 쿼리 파라미터 값을 id 변수에 매핑. id가 없을 수도 있음
+        if (id == null) {   // id가 없으면 글 생성
+            model.addAttribute("article", new ArticleViewResponse());   // 기본 생성자로 비어있는 ArticleViewResponse 객체 만듦
+        } else {    // id가 있으면 글 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 
 }
