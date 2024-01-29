acf8a5b264562d9232a9f1e252da2b138861a216
Hyeju <kimoju03@naver.com>
2024-01-29 21:00:56 +0900

    feat: 토큰 생성 응답 DTO 작성

---
+++ src/main/java/me/hyeju/springboot3blog/dto/CreateAccessTokenResponse.java
@@ -0,0 +1,12 @@
+package me.hyeju.springboot3blog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAccessTokenResponse {
+
+    private String accessToken;
+
+}
