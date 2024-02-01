d78a2df95ee9947d960b7f46aaa888d32ba15ce0
Hyeju <kimoju03@naver.com>
2024-02-01 21:09:24 +0900

    feat: BCryptPasswordEncoder 생성자 수정하고 findByEmail() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/service/UserService.java
+++ src/main/java/me/hyeju/springboot3blog/service/UserService.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import me.hyeju.springboot3blog.domain.User;
 import me.hyeju.springboot3blog.dto.AddUserRequest;
 import me.hyeju.springboot3blog.repository.UserRepository;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +12,22 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public Long save(AddUserRequest request) {
+    public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         return userRepository.save(User.builder()
-                .email(request.getEmail())
-                .password(bCryptPasswordEncoder.encode(request.getPassword()))
+                .email(dto.getEmail())
+                .password(encoder.encode(dto.getPassword()))
                 .build())
                 .getId();
     }
 
     public User findById(Long userId) {
         return userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 
 }
