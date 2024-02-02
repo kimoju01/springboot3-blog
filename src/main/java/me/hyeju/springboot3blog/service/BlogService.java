1f50099e2da4816863b6539bb459eab172d888b6
Hyeju <kimoju03@naver.com>
2024-02-02 20:28:25 +0900

    feat: 글 수정, 삭제 시 게시글을 작성한 유저인지 확인하는 authorizeArticleAuthor() 메서드 작성

--- src/main/java/me/hyeju/springboot3blog/service/BlogService.java
+++ src/main/java/me/hyeju/springboot3blog/service/BlogService.java
@@ -5,6 +5,7 @@ import me.hyeju.springboot3blog.domain.Article;
 import me.hyeju.springboot3blog.dto.AddArticleRequest;
 import me.hyeju.springboot3blog.dto.UpdateArticleRequest;
 import me.hyeju.springboot3blog.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +18,8 @@ public class BlogService {
 
     private final BlogRepository blogRepository;
 
-    public Article save(AddArticleRequest request) {
-        return blogRepository.save(request.toEntity());
+    public Article save(AddArticleRequest request, String userName) {
+        return blogRepository.save(request.toEntity(userName));
     }
 
     // 전체 글 조회
@@ -33,17 +34,29 @@ public class BlogService {
 
     // 글 삭제
     public void delete(Long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     // 글 수정
     @Transactional
     public Article update(Long id, UpdateArticleRequest request) {
         Article article = blogRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
         // 여기서 return blogRepository.save(article); 하지 않아도 업데이트 되는 이유? -> JPA 더티 체킹!
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        // 현재 인증 객체에 담겨져 있는 사용자의 정보와 글을 작성한 사용자의 정보 비교
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 
 }
